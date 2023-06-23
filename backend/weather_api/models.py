1a1f5818d5b331d777d6b038e80b347b333f7a14
Neha Thakur <nehat280@gmail.com>
2023-06-23 10:05:54 +0530

    models.py changes

--- backend/weather_api/models.py
+++ backend/weather_api/models.py
@@ -1,31 +1,30 @@
 from django.db import models
 
 
-class WeatherData(models.Model):
-    region = models.CharField(max_length=50, null=False)
-    parameter = models.CharField(max_length=50, null=False)
-    year = models.IntegerField(default=0,null=False)
-    jan= models.FloatField(default=0,null=True)
-    feb= models.FloatField(default=0,null=True)
-    mar= models.FloatField(default=0,null=True)
-    apr= models.FloatField(default=0,null=True)
-    may= models.FloatField(default=0,null=True)
-    jun= models.FloatField(default=0,null=True)
-    jul= models.FloatField(default=0,null=True)
-    aug= models.FloatField(default=0,null=True)
-    sep = models.FloatField(default=0,null=True)
-    oct= models.FloatField(default=0,null=True)
-    nov= models.FloatField(default=0,null=True)
-    dec= models.FloatField(default=0,null=True)
-    winter = models.FloatField(default=0,null=True)
-    summer = models.FloatField(default=0,null=True)
-    autmn= models.FloatField(default=0,null=True)
-    spring = models.FloatField(default=0,null=True)
-    annual = models.FloatField(default=0,null=True)
+class Region(models.Model):
+    name = models.CharField(max_length=50)
     
-    def __str__(self) -> str:
-        return str(self.year)
-        
+class Parameter(models.Model):
+    parameter_name = models.CharField(max_length=50, unique=True)
+
+class Year(models.Model):
+    year_name = models.IntegerField(default=0)
+
+class Month(models.Model):
+    month_name= models.CharField(max_length=50,unique=True)
+    monthly_data = models.FloatField(null=True)
+    region = models.ForeignKey(Region, on_delete=models.CASCADE)
+    parameter = models.ForeignKey(Parameter, on_delete=models.CASCADE)
+    year = models.ForeignKey(Year, on_delete=models.CASCADE)
+    
     class Meta:
-        unique_together = ('region', 'parameter','year')
+        unique_together = ('region', 'parameter','year','month_name')
     
+    
+class Seasons(models.Model):
+    name = models.CharField(max_length=20, unique=  True)
+    data = models.FloatField(null=True)
+    
+    
+class WeatherData(models.Model):
+    pass
