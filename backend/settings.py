b45a4b2592b7abb5b8ba4f6c9bb33e08007b4f63
Neha Thakur <nehat280@gmail.com>
2023-08-06 22:38:53 +0530

    changes for postgresdb

--- backend/settings.py
+++ backend/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.get('DEBUG', False).lower() == 'true'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env.get('ALLOWED_HOSTS', False).split(" ")
 
 
 # Application definition
