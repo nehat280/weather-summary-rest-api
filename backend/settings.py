3a89a3ad47fff07728728d5bf149b08bc3e9f1e6
Neha Thakur <nehat280@gmail.com>
2023-08-07 10:50:53 +0530

    changes for postgresdb

--- backend/settings.py
+++ backend/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env('SECRET_KEY')
 DEBUG = env.bool('DEBUG', default=False)
 
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=[])
-
+print("ALLOWED_HOSTS",ALLOWED_HOSTS)
 
 # Application definition
 
